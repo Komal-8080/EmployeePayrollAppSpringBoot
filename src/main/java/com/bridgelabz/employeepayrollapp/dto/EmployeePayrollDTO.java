dc044b4dc44285928d3fedc4ac6ede69b2d23ba0
Komal-8080 <shindeg163@gmail.com>
2021-03-12 23:50:33 +0530

    ADDED: VAlidations Added To payroll DTO

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -3,8 +3,7 @@ package com.bridgelabz.employeepayrollapp.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.ToString;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -17,15 +16,21 @@ public @ToString class EmployeePayrollDTO {
     @Min(value = 500, message = "Minimum Wage Should be More then 500")
     public long salary;
 
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
     public String gender;
 
     @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "startDate should Note be Empty")
+    @PastOrPresent(message = "startDate should be past or todays date")
     public LocalDate startDate;
 
+    @NotBlank(message = "Notes cannot be empty")
     public  String note;
 
+    @NotBlank(message = "Profile cannot be empty")
     public String profilePic;
 
+    @NotNull(message = "Department cannot be empty")
     public List<String> departments;
 
 }
