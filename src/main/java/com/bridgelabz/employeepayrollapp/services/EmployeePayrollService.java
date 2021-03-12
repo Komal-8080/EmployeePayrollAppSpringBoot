b69f3d8d079a33fe2ee8ca06831b0b24526937b5
Komal-8080 <shindeg163@gmail.com>
2021-03-13 01:48:42 +0530

    ADDED: All Curd Operations Getting Specific Employee, updating ,deleting developed with MYSQL database

--- src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
@@ -7,8 +7,6 @@ import com.bridgelabz.employeepayrollapp.repository.EmployeePayrollRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,26 +16,22 @@ public class EmployeePayrollService implements IEmployeePayrollService{
     @Autowired
     private EmployeePayrollRepository employeePayrollRepository;
 
-    private List<EmployeePayrollData> empDataList = new ArrayList<>();
-
     @Override
     public List<EmployeePayrollData> getEmployeePayrollData() {
-        return empDataList;
+        return employeePayrollRepository.findAll();
     }
 
     @Override
     public EmployeePayrollData getEmployeePayrollDataById(int empId) {
-        return empDataList.stream()
-                .filter(empData -> empData.getEmpId() == empId)
-                .findFirst()
-                .orElseThrow(() ->new EmployeePayrollException("Employee Not Found"));
+        return employeePayrollRepository
+                .findById(empId)
+                .orElseThrow(() ->new EmployeePayrollException("Employee With EmployeeId"+empId+"does not exists!!"));
     }
 
     @Override
     public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
         EmployeePayrollData empData = null;
         empData = new EmployeePayrollData(empPayrollDTO);
-        empDataList.add(empData);
         log.debug("Emp Data: "+empData.toString());
         return employeePayrollRepository.save(empData);
     }
@@ -45,14 +39,13 @@ public class EmployeePayrollService implements IEmployeePayrollService{
     @Override
     public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
         EmployeePayrollData empData = this.getEmployeePayrollDataById(empId);
-        empData.setName(empPayrollDTO.name);
-        empData.setSalary(empPayrollDTO.salary);
-        empDataList.set(empId-1,empData);
-        return empData;
+        empData.updateEmployeePayrollData(empPayrollDTO);
+        return employeePayrollRepository.save(empData);
     }
 
     @Override
     public void deleteEmployeePayrollData(int empId) {
-        empDataList.remove(empId-1);
+        EmployeePayrollData empData = this.getEmployeePayrollDataById(empId);
+        employeePayrollRepository.delete(empData);
     }
 }
